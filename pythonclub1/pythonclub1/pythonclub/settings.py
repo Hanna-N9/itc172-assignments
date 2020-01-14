03835501349dfb97e3dec463492cedceeba3a541
Hanna-N9 <56139569+Hanna-N9@users.noreply.github.com>
2020-01-14 10:25:01 -0800

    Creating and implementing Models

--- pythonclub1/pythonclub1/pythonclub/settings.py
+++ pythonclub1/pythonclub1/pythonclub/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pythonclubdb',
         'USER' : 'postgres',
-        'PASSWORD': 'P@ssw0rd1',
+        'PASSWORD': 'Greenvampire1',
         'HOST' :'localhost',
         'PORT' :'5433',
     }
